614b40210f4ea65e30000b3c3830186c7bd793a3
mhiremat <mhiremat@Mhiremath-L.gems.glenayre.com>
2020-02-13 22:48:42 +0530

    Added Post request

--- src/test/java/com/api/testng/UsersApi/AssuredAPI.java
+++ src/test/java/com/api/testng/UsersApi/AssuredAPI.java
@@ -2,11 +2,10 @@ package com.api.testng.UsersApi;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
- 
-
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.specification.RedirectSpecification;
 import io.restassured.specification.RequestSpecification;
 
  
@@ -16,18 +15,19 @@ public class AssuredAPI {
     @Test
     public void testapi()
     {
-    	
-        Response resp = RestAssured.get("https://5alive.moviuscorp.net:8021/adk/services/messaging/getorgdetails?organization=2000");
-        int code= resp.getStatusCode();
-        System.out.println(code);
-        //String data = resp.asString();
-        //System.out.println(data);
-        Assert.assertEquals(code,"200");
-        
-        
+    	 RestAssured.baseURI = "";
+         //Request
+         RequestSpecification httprequest = RestAssured.given();
+         
+         //response
+         Response response = httprequest.request(Method.GET,"https://5alive.moviuscorp.net:8021/adk/services/messaging/addressassignments?device_number=916362907347&sip_password=J93cu5f");
+         String responseBody= response.body().asString();
+         System.out.println(responseBody);
+         int code = response.getStatusCode();
+         System.out.print("The Status code is"+code);
+         Assert.assertEquals(200, code); 
+         String Statusline= response.statusLine();
+         System.out.println(Statusline);
     }
     
-
- 
-
 }
