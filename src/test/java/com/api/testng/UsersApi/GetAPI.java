877382e71cea884602d6ebddd50d2485d1e25411
mhiremat <mhiremat@Mhiremath-L.gems.glenayre.com>
2020-02-13 23:01:37 +0530

    Added Verification method for the getAPI

--- src/test/java/com/api/testng/UsersApi/GetAPI.java
+++ src/test/java/com/api/testng/UsersApi/GetAPI.java
@@ -10,7 +10,7 @@ import io.restassured.specification.RequestSpecification;
 
  
 
-public class AssuredAPI {
+public class GetAPI {
     
     @Test
     public void testapi()
@@ -23,11 +23,17 @@ public class AssuredAPI {
          Response response = httprequest.request(Method.GET,"https://5alive.moviuscorp.net:8021/adk/services/messaging/addressassignments?device_number=916362907347&sip_password=J93cu5f");
          String responseBody= response.body().asString();
          System.out.println(responseBody);
+         
+         //header details
+         String contentType= response.header("Content-Type");
+         System.out.println("My content type is "+contentType);
+         Assert.assertEquals(contentType, "application/xml; charset=utf-8");
+        	 
          int code = response.getStatusCode();
-         System.out.print("The Status code is"+code);
+         System.out.print("The Status code is "+code);
          Assert.assertEquals(200, code); 
          String Statusline= response.statusLine();
-         System.out.println(Statusline);
+         System.out.println(" "+Statusline);
     }
     
 }
